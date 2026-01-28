918c89d2210e04b779d5f773c499f83db0a911fb
gustihairi25 <gustihairi25@gmail.com>
2026-01-29 05:10:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+const APP_CONFIG = {
+    // 1. KONEKSI WALLET (Reown / WalletConnect)
+    projectId: 'Cec254c96efb6947d34d6979423c88ce', 
+
+    // 2. PENGATURAN ADMIN
+    adminWallet: '0x4c51175cdf9b6311ce152694f146d29be8911432', 
+    serviceFee: 0.01, // Fee 1%
+
+    // 3. KONFIGURASI BLOCKCHAIN
+    network: {
+        chainId: 7000,
+        rpcUrl: 'https://zetachain-mainnet-archive.allthatnode.com:8545'
+    },
+
+    // 4. KONFIGURASI FIREBASE (Data studio-727777898-40aea)
+    firebase: {
+        apiKey: "AIzaSyDrpBJTtankyLgimtOwuriFpiyuC5K10OI",
+        authDomain: "studio-727777898-40aea.firebaseapp.com",
+        databaseURL: "https://studio-727777898-40aea-default-rtdb.firebaseio.com",
+        projectId: "studio-727777898-40aea",
+        storageBucket: "studio-727777898-40aea.firebasestorage.app",
+        messagingSenderId: "351263967108",
+        appId: "1:351263967108:web:973eb49037d874e5c17fd1",
+        measurementId: "G-C1LTN3E50Q"
+    }
+};
